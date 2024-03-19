9c0e0fd1ec258734f4dd05ba78a9fa8c3a2b6084
Anna-Kyra <anna-kyra.strik@hva.nl>
2024-03-19 10:20:09 +0100

    Opzet node

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log('Hier komt je server')
+import express from 'express'
+import fetchJson from './helpers/fetch-json.js'
+
+const app = express()
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+// ROUTES
+
+app.get('/', function(request, response) {
+	response.render('index')
+})
+
+// Stel het poortnummer in waar express op moet gaan luisteren
+app.set('port', process.env.PORT || 8000)
+
+// Start express op, haal daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function() {
+  // Toon een bericht in de console en geef het poortnummer door
+    console.log(`Application started on http://localhost:${app.get('port')}`)
+})
